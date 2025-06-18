4f86415b27f8653d86d444c9023bf11be348cd29
juderalph20 <edujhplar@gmail.com>
2025-06-19 02:01:12 +0800

    first commit

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'; // <-- Add this
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, RouterModule], // <-- Add RouterModule here
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
